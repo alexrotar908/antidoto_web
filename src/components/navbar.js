2725322e9c5b293cd28e468d4683a9dbf483900a
Alexandru Rotar <alexrotar908@gmail.com>
2025-07-02 13:28:12 +0200

    Actualizo proyecto web

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import './navbar.css';
@@ -8,8 +8,9 @@ const ADMIN_KEY = 'antidotoSuperClave2024';
 function Navbar() {
   const location = useLocation();
   const [isAdmin, setIsAdmin] = useState(false);
-
   const { t, i18n } = useTranslation();
+
+  const activeRef = useRef(null);
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
@@ -24,7 +25,26 @@ function Navbar() {
     }
   }, [location]);
 
-  const adminLink = isAdmin ? <li><Link to={`/admin?key=${ADMIN_KEY}`}>{t('admin')}</Link></li> : null;
+  useEffect(() => {
+    if (activeRef.current) {
+      activeRef.current.scrollIntoView({
+        behavior: 'smooth',
+        inline: 'center',
+        block: 'nearest'
+      });
+    }
+  }, [location.pathname]);
+
+  const adminLink = isAdmin ? (
+    <li>
+      <Link
+        to={`/admin?key=${ADMIN_KEY}`}
+        ref={location.pathname === "/admin" ? activeRef : null}
+      >
+        {t('admin')}
+      </Link>
+    </li>
+  ) : null;
 
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
@@ -32,20 +52,48 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-     <ul className="nav-links">
-      <li><Link to="/">{t('inicio')}</Link></li>
-      <li><Link to="/restaurante">{t('restaurante.title')}</Link></li>
-      <li><Link to="/reservas">{t('reservas')}</Link></li>
-      <li><Link to="/sobre-nosotros">{t('sobreNosotros')}</Link></li>
-           {adminLink}
-     </ul>
+      <ul className="nav-links">
+        <li>
+          <Link
+            to="/"
+            ref={location.pathname === "/" ? activeRef : null}
+          >
+            {t('inicio')}
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/restaurante"
+            ref={location.pathname === "/restaurante" ? activeRef : null}
+          >
+            {t('restaurante.title')}
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/reservas"
+            ref={location.pathname === "/reservas" ? activeRef : null}
+          >
+            {t('reservas')}
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/sobre-nosotros"
+            ref={location.pathname === "/sobre-nosotros" ? activeRef : null}
+          >
+            {t('sobreNosotros')}
+          </Link>
+        </li>
+        {adminLink}
+      </ul>
 
-     <div className="language-selector">
-       <button onClick={() => changeLanguage('es')}>{t('espanol')}</button>
-       <button onClick={() => changeLanguage('en')}>{t('ingles')}</button>
-     </div>
+      <div className="language-selector">
+        <button onClick={() => changeLanguage('es')}>{t('espanol')}</button>
+        <span className="language-separator">|</span>
+        <button onClick={() => changeLanguage('en')}>{t('ingles')}</button>
+      </div>
     </nav>
-
   );
 }
 
